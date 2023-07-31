fbcacf049d5656cd7767583ce2e6065f817256d9
blazama <blancazapicom@gmail.com>
2023-07-31 10:41:37 +0200

    Added useState and values

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const state = useState([]);
+  const value = state[0];
+  const updateValue = state[1];
+
   return (
     <div className="App">
       <section className="App-content">
