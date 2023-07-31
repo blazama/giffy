35785a6eb6026ec1322c8f30af1fab8b745803e0
blazama <blancazapicom@gmail.com>
2023-07-31 10:53:19 +0200

    Added a panda gif

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const state = useState([]);
-  const value = state[0];
-  const updateValue = state[1];
+  const [value, updateValue] = useState(['https://media0.giphy.com/media/BCgcfnaGDaYai5eYD1/giphy.gif?cid=ecf05e47lc9w230k2kq7zatp7cou9oqdxx0atvut7enx3jo6&ep=v1_gifs_search&rid=giphy.gif&ct=g']);
 
   return (
     <div className="App">
       <section className="App-content">
       
-        <p>
-         Gifs
-        </p>
-
+        <img src={value} alt='gif'/>
       </section>
     </div>
   );
